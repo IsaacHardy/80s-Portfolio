814c6504e4e2358add157b431045a79ed78b2301
Isaac Hardy <isaac@isaachardy.io>
2017-08-29 10:49:18 -0400

    implemented star rotation to use two different speeds to demonstrated an active sky

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,8 +6,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let getAnimationStyle = (num) => {
+      let style = {
+        "animation": "rotate " + num + "s" + " infinite linear",
+        "-webkit-animation": "rotate " + num + "s" + " infinite linear"
+      };
+      return style;
+    };
+
     let generateStars = () => {
-      let amount = 250;
+      let amount = 125;
       let stars = [];
 
       for (let i = 0; i < amount; i++) {
@@ -16,6 +24,7 @@ class App extends Component {
 
       return stars;
     };
+
     return (
       <div className="App">
         <nav className="nav">
@@ -23,7 +32,10 @@ class App extends Component {
           <a target="_blank" rel="noopener noreferrer" className="gitHub" href="https://github.com/IsaacHardy"><i className="fa fa-github" aria-hidden="true"></i></a>
           <a target="_blank" rel="noopener noreferrer" className="linkedIn" href="https://www.linkedin.com/in/isaachardy/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
         </nav>
-        <div className="starContainer">
+        <div className="starContainer" style={getAnimationStyle(200)}>
+          {generateStars()}
+        </div>
+        <div className="starContainer" style={getAnimationStyle(145)}>
           {generateStars()}
         </div>
         <Sun />
